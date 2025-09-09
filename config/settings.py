e4f415de0394231870c2cc0a0bd45619be17d9a8
João Pedro <joaop@dpsatacado.com.br>
2025-09-09 13:56:56 -0300

    Config: Adiciona o hostname da Render a ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-t0t@%l&57%(=y%khm^c4s9wxz(nx$#&3wex11@ezq+r7^o$v6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
